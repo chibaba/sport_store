f55c22bda23d3eda226f29336ff5e88f68856158
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-03-03 19:07:16 +0100

    created action types for cart action creators

--- src/shopFolder/CategoryNavigation.js
+++ src/shopFolder/CategoryNavigation.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
+import { ToggleLink } from "../ToggleLink"
 
 export class CategoryNavigation extends Component {
     render() {
         return <React.Fragment>
-            <Link className="btn btn-secondary btn-block"
-                to={this.props.baseUrl}>All</Link>
-            to={this.props.categories && this.props.categories.map
-                (cat => <Link className="btn btn-secondary btn-block"
+            <ToggleLink to={this.props.baseUrl } exact={ true }>ALL</ToggleLink>
+            {this.props.categories && this.props.categories.map
+                (cat => <ToggleLink 
                     key={cat}
                     to={`${this.props.baseUrl}/${cat.toLowerCase()}`}>
                     {cat}
-                </Link>
+                    </ToggleLink> 
                 )}
         </React.Fragment>
     }
